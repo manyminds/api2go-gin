16de6c56f9dcab84d93bf3346c24db537fd42818
Andy Whittle <andy.whittle@fatsoma.com>
2017-02-14 14:50:29 +0000

    Support passing whitelisted routes to router
    This allows a whitelist of valid prefixes to the router, it will then
    skip adding the routes that are not present in the list.

--- gingonic/gingonic.go
+++ gingonic/gingonic.go
@@ -2,13 +2,15 @@ package gingonic
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/manyminds/api2go/routing"
 )
 
 type ginRouter struct {
-	router *gin.Engine
+	router    *gin.Engine
+	whitelist []string
 }
 
 func (g ginRouter) Handler() http.Handler {
@@ -16,19 +18,38 @@ func (g ginRouter) Handler() http.Handler {
 }
 
 func (g ginRouter) Handle(protocol, route string, handler routing.HandlerFunc) {
-	wrappedCallback := func(c *gin.Context) {
-		params := map[string]string{}
-		for _, p := range c.Params {
-			params[p.Key] = p.Value
+	if g.whiteListed(route) {
+		wrappedCallback := func(c *gin.Context) {
+			params := map[string]string{}
+			for _, p := range c.Params {
+				params[p.Key] = p.Value
+			}
+
+			handler(c.Writer, c.Request, params)
 		}
 
-		handler(c.Writer, c.Request, params)
+		g.router.Handle(protocol, route, wrappedCallback)
+	}
+}
+
+func (g ginRouter) whiteListed(route string) bool {
+	if len(g.whitelist) == 0 {
+		// if the white list is omitted all routes are valid
+		return true
 	}
 
-	g.router.Handle(protocol, route, wrappedCallback)
+	// scan through the white list for allowed route prefixes
+	for _, allowed := range g.whitelist {
+		if strings.HasPrefix(route, allowed) {
+			return true
+		}
+	}
+
+	// not found in white list
+	return false
 }
 
 //New creates a new api2go router to use with the gin framework
-func New(g *gin.Engine) routing.Routeable {
-	return &ginRouter{router: g}
+func New(g *gin.Engine, w ...string) routing.Routeable {
+	return &ginRouter{router: g, whitelist: w}
 }
